82a7ee355a0e791b495f5a3440397f62f8678c2b
vasu <vasanthakumarnr6@gmail.com>
2022-09-20 10:37:03 -0700

    added - contact details

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -3,6 +3,8 @@ public class AddressBookMain {
     public static void main(String args[]) {
 
         System.out.println("Welcome to Address Book Program");
+        contact contact = new contact();
+        contact.setContactDetails();
 
     }
 }
